c435ad639b2eb71e82cc18c2f7533951e04f7d89
ltran <tranln025@gmail.com>
2019-11-20 14:24:37 -0800

    Adds routes

---
+++ src/config/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Splash from '../components/Splash/Splash';
+import Profile from '../components/Profile/Profile';
+// import City from '../components/City/City';
+// import Post from '../components/Post/Post';
+
+export default () => {
+  <Switch>
+    <Route exact path="/" component={Splash} />
+    <Route path="/users/:id" component={Profile} />
+    {/* <Route path="/cities/:city_id" component={City} />
+    <Route path="/posts/:post_id" component={Post} /> */}
+  </Switch>
+}
