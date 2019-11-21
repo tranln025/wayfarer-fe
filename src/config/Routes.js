4953f62b0e7dad4be7557fa240371bd83f2dafd7
AliStoltz <ali.mstoltz@gmail.com>
2019-11-21 14:50:58 -0800

    added link to post detailsfrom profile

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import Splash from '../components/Splash/Splash';
 import ProfileContainer from '../containers/ProfileContainer/ProfileContainer';
 // import City from '../components/City/City';
-// import Post from '../components/Post/Post';
+import Post from '../components/Post/Post';
 
 import './Routes.css'
 
@@ -13,8 +12,8 @@ export default ({ currentUser, setCurrentUser }) => (
     <Switch>
       <Route exact path="/"  render={() => <Splash currentUser={currentUser} setCurrentUser={setCurrentUser}/>} />
       <Route path="/users/:id" component={ProfileContainer} />
-      {/* <Route path="/cities/:cityId" component={City} />
-      <Route path="/posts/:postId" component={Post} /> */}
+      {/* <Route path="/cities/:cityId" component={City} /> */}
+      <Route path="/post/:postId" component={Post} />
     </Switch>
   </div>
 )
