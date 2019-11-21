d0898e8375de9892b727744318e4a402aa980459
ltran <tranln025@gmail.com>
2019-11-20 21:46:02 -0800

    Adds Profile and ProfileContainer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Routes from './config/Routes';
+import axios from 'axios';
 
 import Navbar from './Navbar/Navbar';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <h1>Ali Sux</h1>
-      <Routes />
-    </div>
-  );
+class App extends Component {
+  state = {
+    currentUser: localStorage.getItem('uid'),
+  };
+
+  setCurrentUser = (userId) => {
+    this.setState({ currentUser: userId });
+    localStorage.setItem('uid', userId);
+  };
+
+  logout = () => {
+    localStorage.removeItem('uid');
+    axios.post(`${process.env.REACT_APP_API_URL}/auth/logout`, { withCredentials: true })
+      .then(res => {
+        console.log(res);
+        this.setState({ currentUser: null });
+        this.props.history.push('/login');
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1>Ali Sux</h1>
+        <Routes />
+      </div>
+    );
+  }
 }
 
 export default App;
