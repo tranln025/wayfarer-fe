6b4be9ec1d19a86eb23d28bbf9f6036e9c4b1417
ltran <tranln025@gmail.com>
2019-11-20 16:49:50 -0800

    Adds handleModalOpen function

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Splash from '../components/Splash/Splash';
-import Profile from '../components/Profile/Profile';
+// import Profile from '../components/Profile/Profile';
 // import City from '../components/City/City';
 // import Post from '../components/Post/Post';
 
-export default () => {
+export default () => (
   <Switch>
     <Route exact path="/" component={Splash} />
-    <Route path="/users/:id" component={Profile} />
-    {/* <Route path="/cities/:city_id" component={City} />
-    <Route path="/posts/:post_id" component={Post} /> */}
+    {/* <Route path="/users/:id" component={Profile} /> */}
+    {/* <Route path="/cities/:cityId" component={City} />
+    <Route path="/posts/:postId" component={Post} /> */}
   </Switch>
-}
+)
