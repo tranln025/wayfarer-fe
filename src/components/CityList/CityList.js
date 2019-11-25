9def3a1c7e0c201fb15fe49b56bff5f43ada17b7
tanyakelly <murenkot@gmail.com>
2019-11-25 12:24:02 -0800

    carusel photos

--- src/components/CityList/CityList.js
+++ src/components/CityList/CityList.js
@@ -24,8 +24,6 @@ const CityList = (props) => {
                                         </div>
                                     </ListGroup.Item>
                                 </Link>
-                                
-                 
                             ) 
                         })}
                     </ListGroup>
