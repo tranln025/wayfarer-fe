ae3cb5309ca952a3d3f27ec923b11618d0aad635
ltran <tranln025@gmail.com>
2019-11-25 23:50:06 -0800

    Styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,16 +3,16 @@ import './About.css';
 
 
 const About = () => (
-<div className="container">
-  <div className="topics">
+<div className="container topics">
+  <div className="topic">
     <h3>Topic</h3>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa minima deleniti recusandae quod, quae asperiores nesciunt aspernatur necessitatibus. Reiciendis dolorem, odit magni dolores alias doloremque laboriosam nisi non eveniet numquam!</p>
   </div>
-  <div className="topics">
+  <div className="topic">
     <h3>Topic</h3>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa minima deleniti recusandae quod, quae asperiores nesciunt aspernatur necessitatibus. Reiciendis dolorem, odit magni dolores alias doloremque laboriosam nisi non eveniet numquam!</p>
   </div>
-  <div className="topics">
+  <div className="topic">
     <h3>Topic</h3>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa minima deleniti recusandae quod, quae asperiores nesciunt aspernatur necessitatibus. Reiciendis dolorem, odit magni dolores alias doloremque laboriosam nisi non eveniet numquam!</p>
   </div>
--- src/components/CityList/CityList.js
+++ src/components/CityList/CityList.js
@@ -15,7 +15,7 @@ const CityList = (props) => {
                         {props.cityList && props.cityList.map((city, index)=> {
                             return (
                                 <Link name={city.name} onClick={()=>props.handleSelect(city.name)}  style={{ textDecoration: 'none' }} to={"/cities/" + city.name.toLowerCase().split(' ').join('-')}>
-                                    <ListGroup.Item  name={city.name} onClick={()=>props.handleSelect(city.name)} className="list-item" variant="secondary" action href={"/cities/" + city.name.toLowerCase().split(' ').join('-')} >
+                                    <ListGroup.Item  name={city.name} onClick={()=>props.handleSelect(city.name)} className="list-item" action href={"/cities/" + city.name.toLowerCase().split(' ').join('-')} >
                                         <div name={city.name} onClick={()=>props.handleSelect(city.name)} className="city-container row">
                                             <div name={city.name} onClick={()=>props.handleSelect(city.name)} className="city-photo-container col-5">
                                                 <img src={city.photo} alt="city" />
