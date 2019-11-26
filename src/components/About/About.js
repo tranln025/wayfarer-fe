8d73a42d669b707bef85259289b914033739bc40
AliStoltz <ali.mstoltz@gmail.com>
2019-11-26 10:08:39 -0800

    updates lorem

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => (
   </div>
   <div className="topic">
     <h3>Topic</h3>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa minima deleniti recusandae quod, quae asperiores nesciunt aspernatur necessitatibus. Reiciendis dolorem, odit magni dolores alias doloremque laboriosam nisi non eveniet numquam!</p>
+    <p>Lorem ipsum sit amet consectetur, adipisicing elit. Culpa minima deleniti recusandae quod, quae asperiores nesciunt aspernatur necessitatibus. Reiciendis dolorem, odit magni dolores alias doloremque laboriosam nisi non eveniet numquam!</p>
   </div>
   <div className="topic">
     <h3>Topic</h3>
